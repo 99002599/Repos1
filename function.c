82878c02360d4a35333ab8e3e79f8ff1c615667d
99002599 <72463292+99002599@users.noreply.github.com>
2020-10-09 09:34:03 +0530

    Add files via upload

---
+++ function.c
@@ -0,0 +1,7 @@
+
+#include <stdio.h>
+#include "headers.h"
+void printFunction()
+{
+    printf("Hello World");
+}
---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "headers.h"
+main()
+{
+    printfunction();
+}
